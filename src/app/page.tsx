fb76a6f60d6139055e5d9c2bb245f4c8b692cdcb
ebiyy <kuraim22@gmail.com>
2025-01-18 07:24:19 +0900

    fix: 環境変数の名前を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,10 @@ const initializeAPI = async () => {
     environment:
       (process.env.NODE_ENV as "development" | "staging" | "production") ??
       "development",
-    databaseUrl: process.env.SUPABASE_URL ?? "http://127.0.0.1:54321",
+    databaseUrl:
+      process.env.NEXT_PUBLIC_SUPABASE_URL ?? "http://127.0.0.1:54321",
     supabaseAnonKey:
-      process.env.SUPABASE_ANON_KEY ??
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
   });
 };
